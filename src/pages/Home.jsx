7fbad64932a4b327554d92af8f00f38cd5953a46
ArindamBiswas10 <arindombiswas29@gmail.com>
2026-01-13 23:09:19 +0530

    Updated everything

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,6 @@ const Home = () => {
             <HomeHeroText/>
             <HomeBottomText/>
         </div>
-        <div className='h-screen w-screen fixed top-0 left-0 bg-black/60 pointer-events-none'></div>
     </div>
   )
 }
